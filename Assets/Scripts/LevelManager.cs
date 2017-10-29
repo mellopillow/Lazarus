a855776e6480c60030702e012280d58b5cc7c02e
Anthony Boozan <aboozan@uci.edu>
2017-10-29 01:57:28 -0700

    Omg its playable! added scene movement so the character moves from scene to scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,23 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
-
 	public void LoadLevel(string name)
     {
         Debug.Log("Level load requested for: " + name);
-        Application.LoadLevel(name);  
+        SceneManager.LoadScene(name);
     }
 
     public void QuitRequest()
     {
         Debug.Log("Quit Game");
         Application.Quit();
-    }
-    public void LoadNextLevel()
-    {
-        Application.LoadLevel(Application.loadedLevel + 1);
     }
     
 }
