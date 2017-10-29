399bdaf10222a307ea2637b882974db489c5577a
Anthony Boozan <aboozan@uci.edu>
2017-10-28 23:31:56 -0700

    added music and level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);  
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+    
+}
