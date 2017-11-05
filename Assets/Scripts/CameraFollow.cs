32f06549288ce077cba264390fd77ff93d1c79fb
Unknown <richmonl@uci.edu>
2017-11-05 13:37:23 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    public bool bounds;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        if (bounds)
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x),
+                Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y),
+                Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
+        }
+    }
+
+}
