1316ad06d8da9d6e79000d79aa01708067f50f02
Unknown <emombay@uci.edu>
2017-11-06 14:05:15 -0800

    Pause menu in progress

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public UIManager UI;
+    public GameObject PauseMenu;
+    private bool paused;
+    public static GameManager instance = null;
+
+
+
+    // Use this for initialization
+    void Start () {
+        //PauseMenu.gameObject.SetActive(false);
+
+        Debug.Log("Start");
+        //Check for AudioManager
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        //Use if you don't want to destroy between scenes.
+        DontDestroyOnLoad(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TogglePauseMenu()
+    {
+        if (paused)
+        {
+            paused = false;
+            Pause();
+        }
+        else
+        {
+            paused = true;
+            Unpause();
+        }
+    }
+
+    public void Pause()
+    {
+        Debug.Log("paused");
+        Time.timeScale = 0;
+        PauseMenu.SetActive(true);
+        Debug.Log("set active");
+    }
+
+    public void Unpause()
+    {
+        Debug.Log("paused");
+        Time.timeScale = 1;
+        PauseMenu.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public GameManager GM;
+    
+    GameObject SceneText;
+    GameObject GameText;
+
+
+	// Use this for initialization
+	void Start () {
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ScanForKeyStroke();
+	}
+
+    void ScanForKeyStroke()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            GM.TogglePauseMenu();
+        }
+    }
+}
