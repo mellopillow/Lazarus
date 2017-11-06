f9f6488ca163b877bd60463640b60dbb1a5b8a84
Anthony Boozan <aboozan@uci.edu>
2017-11-05 22:17:31 -0800

    Destroy bullets on collision

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour {
 
-	// Use this for initialization
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Destoryed");
