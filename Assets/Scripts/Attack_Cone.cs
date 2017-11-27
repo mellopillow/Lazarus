060d1c59ab8d16b0925724423b1286c062903e52
Unknown <richmonl@uci.edu>
2017-11-27 00:27:09 -0800

    Barebones turret
    Has a cone-shaped detection range; fires when player enters the cone

---
+++ Assets/Scripts/Attack_Cone.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Cone : MonoBehaviour {
+
+    public TurretAI turretAI;
+
+    public bool isLeft = false;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        turretAI = gameObject.GetComponentInParent<TurretAI>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (collision.CompareTag("Player"))
+        {
+
+            if (isLeft)
+            {
+
+                turretAI.Attack(false);
+
+            }
+            else
+            {
+
+                turretAI.Attack(true);
+
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/TurretAI.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour {
+
+
+    public float distance;
+    public float shootInterval;
+    public float projSpeed;
+    public float projTimer;
+
+    public bool lookingRight = true;
+
+    public GameObject bullet;
+    public Transform target;
+    public Transform shootPointLeft, shootPointRight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //For animations
+        /*
+        RangeCheck();
+
+        if (target.transform.position.x > transform.position.x)
+        {
+            lookingRight = true;
+        }
+
+
+        if (target.transform.position.x < transform.position.x)
+        {
+            lookingRight = false;
+        }
+        */
+	}
+
+    //For animations
+    /*
+    void RangeCheck()
+    {
+
+        distance = Vector3.Distance(transform.position, target.transform.position);
+
+
+    }
+    */
+
+    public void Attack(bool attackingRight)
+    {
+
+        projTimer += Time.deltaTime;
+
+        if (projTimer >= shootInterval)
+        {
+
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
+
+            if (!attackingRight)
+            {
+                GameObject bulletClone;
+                bulletClone = Instantiate(bullet, shootPointLeft.transform.position, shootPointLeft.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * projSpeed;
+
+                projTimer = 0;
+            }
+
+            if (attackingRight)
+            {
+                GameObject bulletClone;
+                bulletClone = Instantiate(bullet, shootPointRight.transform.position, shootPointRight.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * projSpeed;
+
+                projTimer = 0;
+            }
+
+
+        }
+
+    }
+}
