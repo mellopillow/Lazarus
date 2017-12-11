cc7363bcffbdd5a31e75a1f27e81b14f7b3c153a
Anthony Boozan <aboozan@uci.edu>
2017-12-11 01:41:31 -0800

    AI changes

--- Assets/Scripts/FlyingEnemy_v2.cs
+++ Assets/Scripts/FlyingEnemy_v2.cs
@@ -20,7 +20,8 @@ public class FlyingEnemy_v2 : MonoBehaviour {
 	bool attacking = false;
 	// Use this for initialization
 	void Start () {
-		target = GameObject.FindGameObjectWithTag ("Player").transform;
+        
+		
 		health = GetComponent<Health> ();
 	//	SoundPlayer2D.clip = sound;
 
@@ -28,32 +29,45 @@ public class FlyingEnemy_v2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//SoundPlayer2D.Play ();
-		if (health.dead) {
-			Instantiate (deathParticle, transform.position, transform.rotation);
-			Destroy (gameObject);
-		}
-		//AudioManager.instance.soundfx (10, 0.3f);
-		playerInRange = Physics2D.OverlapCircle (transform.position, playerRange, playerLayer);
-		Vector2 direction = target.transform.position - transform.position;
-		direction.Normalize();
+        //SoundPlayer2D.Play ();
+        try
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+            if (health.dead)
+            {
+                Instantiate(deathParticle, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+            //AudioManager.instance.soundfx (10, 0.3f);
+            playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
 
-		Vector3 newScale;
-		if (playerInRange) {
-			
-			if (direction.x < 0f) { //facing left
-				newScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
-				transform.localScale = newScale;
-			}
+            Vector3 newScale;
+            if (playerInRange)
+            {
 
-			if (direction.x > 0f) { //facing right
-				newScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-				transform.localScale = newScale;
-			}
+                if (direction.x < 0f)
+                { //facing left
+                    newScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                    transform.localScale = newScale;
+                }
 
-			transform.position = Vector3.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+                if (direction.x > 0f)
+                { //facing right
+                    newScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                    transform.localScale = newScale;
+                }
 
-		}
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+            }
+        }
+        catch
+        {
+           
+        }
+        
 	}
 
 
